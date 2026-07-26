dc0ceb51ed2129a57190ee2ba78416dc524f9216
shlee <shlee@nshc.net>
2026-07-26 19:00:51 +0900

    (perf) BasicsScreen Preview 추가

--- app/src/main/java/com/asad/composeuipractice/screens/BasicsScreen.kt
+++ app/src/main/java/com/asad/composeuipractice/screens/BasicsScreen.kt
@@ -32,7 +32,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.asad.composeuipractice.ui.theme.ComposeUiPracticeTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -154,5 +156,13 @@ fun BasicsScreen() {
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BasicsScreenPreview() {
+    ComposeUiPracticeTheme {
+        BasicsScreen()
     }
 }
